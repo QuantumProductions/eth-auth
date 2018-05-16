e8e18a870e9e51750e9a4ae7491b98cdbac6cb53
quantumpotato <quantumpotato@gmail.com>
2018-05-15 20:52:43 -0400

    Working prototype

--- index.js
+++ index.js
@@ -3,29 +3,30 @@ const BigNumber = require('bignumber.js');
 const sigUtil = require('eth-sig-util');
 
 function authenticate(internalUserIdentifier = 'username123', identityPhrase = 'user-token', 
-    ethereumAddress = '0xWhoTheyClaimToBe', signedData = 'data-signed-by-users-wallet-expected-to-match-user-token',
-    callback) {
-
+    ethereumAddress = '0xWhoTheyClaimToBe', signedData = 'data-signed-by-users-wallet-expected-to-match-user-token') {
 
     let statement = [{ type: 'string', name: 'eth-auth', value: identityPhrase}];
-    let from = ethereumAddress.toLowerCase(); //inconsistencies in Metamask address display + the checksumming of sigUtil.. TODO: may need to check into this
+    console.log("statement" + JSON.stringify(statement));
+    let from = ethereumAddress.toLowerCase();
+    //inconsistencies in Metamask address display + the checksumming of sigUtil.. 
+    //sigutil will recover message returning address as lowercase
 
     if (sig.length < 10) {
-      callback({error: {message: "Invalid signature length", internalUserIdentifier: internalUserIdentifier, ethereumAddress: ethereumAddress}});
-      return;
+      return {success: null, error: {message: "Invalid signature length", internalUserIdentifier: internalUserIdentifier, ethereumAddress: ethereumAddress}};
     }
     
     let rtsParam = { data: statement, sig: signedData };
 
     try {
       const recovered = sigUtil.recoverTypedSignature(rtsParam);
+      console.log("Recovered " + recovered);
       if (recovered === from ) {
-        callback({success: {internalUserIdentifier: internalUserIdentifier, recovered: recovered, identityPhrase: identityPhrase, ethereumAddress: ethereumAddress}});
+        return {error: null, success: {internalUserIdentifier: internalUserIdentifier, recovered: recovered, identityPhrase: identityPhrase, ethereumAddress: ethereumAddress}};
       } else {
-        callback({error: {message: "Invalid Signature", invalidData: recovered, internalUserIdentifier: internalUserIdentifier, ethereumAddress: ethereumAddress}});
+        return {success: null, error: {message: "Invalid Signature", invalidData: recovered, internalUserIdentifier: internalUserIdentifier, ethereumAddress: ethereumAddress}};
       }
     } catch (ex) {
-      callback({error: {message: "Error parsing signature", internalUserIdentifier: internalUserIdentifier, ethereumAddress: ethereumAddress}});
+      return {success: null, error: {message: "Error parsing signature", internalUserIdentifier: internalUserIdentifier, ethereumAddress: ethereumAddress}};
     }
 }
 
